7ce1d14999db5212f6e8769f918b8c186156f5e0
Bruno Tavares <brunustavares@gmail.com>
2025-12-22 15:32:39 +0000

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import CONFIG from './gitprofile.config.ts';
 import GitProfile from './components/gitprofile.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
