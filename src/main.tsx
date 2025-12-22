492bea9c4b2ff0c6c5f6caac56fdde8409c81c7c
Bruno Tavares <brunustavares@gmail.com>
2025-12-22 20:16:50 +0000

    WakaTime output revision

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import GitProfile from './components/gitprofile.tsx';
+import GitProfile from './components/gitprofile';
+import CONFIG from '../gitprofile.config';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <GitProfile config={CONFIG} />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ import { createHtmlPlugin } from 'vite-plugin-html';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: CONFIG.base || '/',
+//   base: CONFIG.base || '/',
+  base: '/',
   plugins: [
     react(),
     createHtmlPlugin({
